760a0e4059e317fb4890a1041b4d1c6324bb3f06
loopfagundes <lof.fagundes@gmail.com>
2024-11-29 13:23:32 -0300

    adicionei os pacotes de step,url e reports configurada

---
+++ src/main/java/com/saucedemo/utils/Screenshot.java
@@ -0,0 +1,22 @@
+package com.saucedemo.utils;
+
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.service.ExtentTestManager;
+import com.saucedemo.webdrivers.DriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Screenshot {
+    public static MediaEntityModelProvider capture() {
+        String base64image = ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BASE64);
+
+        try {
+            return MediaEntityBuilder.createScreenCaptureFromBase64String(base64image).build();
+        } catch (Exception e) {
+            ExtentTestManager.getTest().log(Status.FAIL, "Não foi possível gerar uma evidência!");
+        }
+        return null;
+    }
+}
