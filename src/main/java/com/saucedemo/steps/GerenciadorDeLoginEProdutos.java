2896683676b510bde48ad7c6c3395ea3c8a4854c
loopfagundes <lof.fagundes@gmail.com>
2024-11-29 16:56:42 -0300

    renomes class e metodos para melhorar entendimento.

--- src/main/java/com/saucedemo/steps/GerenciadorDeLoginEProdutos.java
+++ src/main/java/com/saucedemo/steps/GerenciadorDeLoginEProdutos.java
@@ -3,31 +3,42 @@ package com.saucedemo.steps;
 import com.aventstack.extentreports.Status;
 import com.saucedemo.dto.UserDataDTO;
 import com.saucedemo.pageobjects.LoginAppObject;
+import com.saucedemo.pageobjects.ProductsAppObject;
 import com.saucedemo.utils.Report;
 import org.openqa.selenium.WebDriver;
 
-public class LoginERealizarACompraStep {
+public class GerenciadorDeLoginEProdutos {
     private WebDriver driver;
     private final LoginAppObject loginUsuario;
+    private final ProductsAppObject products;
 
-    public LoginERealizarACompraStep(WebDriver _driver) {
+    public GerenciadorDeLoginEProdutos(WebDriver _driver) {
         driver = _driver;
         loginUsuario = new LoginAppObject(_driver);
+        products = new ProductsAppObject(_driver);
     }
 
-    public void EfeturaLoginEFInalizarCompra() {
-        EfeturaLogin();
+    public void iniciaFluxoDeCompra() {
+        efeturaLogin();
+        adicionaItensAoCarrinho();
     }
 
-    private void EfeturaLogin() {
+    private void efeturaLogin() {
         Report.log(Status.INFO,"Acessar na tela de login");
         loginUsuario.usernameTextField().sendKeys(UserDataDTO.userData().getUsername());
-        loginUsuario.passwordTextField().sendKeys("test");
+        loginUsuario.passwordTextField().sendKeys(UserDataDTO.userData().getPassword());
         if(loginUsuario.loginButton().isDisplayed()) {
             loginUsuario.loginButton().click();
             Report.log(Status.PASS, "O botão 'Login' recebeu um clique.");
         } else {
             Report.logCapture(Status.FAIL, "O botão 'Login' não recebeu um clique");
         }
+    }
+
+    private void adicionaItensAoCarrinho() {
+        Report.log(Status.INFO, "Redirecionado para tela de Products");
+        products.mochilaProdutoAddToCartButton().click();
+        products.jaquetaProdutoAddToCartButton().click();
+        System.out.println("CARRINHO:" + products.quantidadesDosProdutosNoIconeDoCarrinhoLabel().getText());
     }
 }
