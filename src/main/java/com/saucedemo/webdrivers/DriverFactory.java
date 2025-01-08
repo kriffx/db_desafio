c413dc370842c1f99f9f65c901d77c093757a98c
Luciano <lof.fagundes@gmail.com>
2025-01-08 20:54:32 -0300

    remove headless

--- src/main/java/com/saucedemo/webdrivers/DriverFactory.java
+++ src/main/java/com/saucedemo/webdrivers/DriverFactory.java
@@ -7,49 +7,27 @@ import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverFactory {
 
     @Setter
     private static ExtentTest test;
-    public static boolean isHeadless;
 
     public static WebDriver createInstance(BrowserEnum browser) {
 
-        String headless = System.getProperty("headless", "false").toLowerCase();
-        if (headless.equals("true") || headless.equals("false")) {
-            isHeadless = Boolean.parseBoolean(headless);
-        } else {
-            throw new IllegalArgumentException("The headless argument only receives true or false!");
-        }
 
         try {
             switch (browser) {
                 case FIREFOX:
                     WebDriverManager.firefoxdriver().setup();
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    if (isHeadless) {
-                        firefoxOptions.addArguments("--headless");
-                    }
                     return new FirefoxDriver();
                 case CHROME:
                     WebDriverManager.chromedriver().setup();
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    if (isHeadless) {
-                        chromeOptions.addArguments("--headless");
-                    }
                     return new ChromeDriver();
                 case EDGE:
                     WebDriverManager.edgedriver().setup();
-                    EdgeOptions edgeOptions = new EdgeOptions();
-                    if (isHeadless) {
-                        edgeOptions.addArguments("--headless");
-                    }
                     return new EdgeDriver();
                 default:
                     throw new IllegalArgumentException("Navegador inválido:" + browser);
